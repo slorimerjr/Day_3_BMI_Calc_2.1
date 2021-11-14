bc745a3e00c346f46b91e3eb269cf337a1c13966
Sam Lorimer <slorimerjr@gmail.com>
2021-11-14 20:40:38 +0000

    Completed challenge.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-bmi = round(float(weight / height**2), 1)
+bmi = round(weight / height ** 2)
 
 if bmi < 18.5:
   print(f"Your BMI is {bmi}, you are underweight.")
@@ -18,5 +18,16 @@ else:
   print(f"Your BMI is {bmi}, you are clinically obese.")
 
 
-
+# Teacher's Solution
+bmi = round(weight / height ** 2)
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
 
